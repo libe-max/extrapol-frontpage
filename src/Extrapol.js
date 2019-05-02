7f0508d933d41070e425a83196edadab84d3b7b8
Maxime Fabas <m.fabas@liberation.fr>
2019-05-02 12:45:41 +0200

    Ready

--- src/Extrapol.js
+++ src/Extrapol.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react'
+import moment from 'moment'
+import 'moment/locale/fr'
 import PageTitle from 'libe-components/lib/text-levels/PageTitle'
 import Hat from 'libe-components/lib/text-levels/Hat'
+import BlockTitle from 'libe-components/lib/text-levels/BlockTitle'
+import Slug from 'libe-components/lib/text-levels/Slug'
 import Paragraph from 'libe-components/lib/text-levels/Paragraph'
+import Annotation from 'libe-components/lib/text-levels/Annotation'
+import Loader from 'libe-components/lib/blocks/Loader'
+import LoadingError from 'libe-components/lib/blocks/LoadingError'
+import LibeLaboLogo from 'libe-components/lib/blocks/LibeLaboLogo'
 import { parseTsvWithTabs } from 'libe-utils'
 
+import VideoSlot from './components/VideoSlot'
+
 export default class Extrapol extends Component {
+  /* * * * * * * * * * * * * * * * * *
+   *
+   * CONSTRUCTOR
+   *
+   * * * * * * * * * * * * * * * * * */
   constructor () {
     super()
     this.c = 'extrapol'
     this.state = {
       loading: true,
       error: null,
-      data: null
+      data: {
+        videos: [],
+        page: {},
+        subjects: []
+      }
     }
   }
 
+  /* * * * * * * * * * * * * * * * * *
+   *
+   * DID MOUNT
+   *
+   * * * * * * * * * * * * * * * * * */
   componentDidMount () {
     const { spreadsheet } = this.props
     window.fetch(spreadsheet).then(res => {
@@ -24,17 +48,23 @@ export default class Extrapol extends Component {
       const parsed = parseTsvWithTabs({
         tsv: text,
         tabsParams: [
-          { start: 0, end: 2, keysLinePos: 1 },
-          { start: 3, end: 5, keysLinePos: 1 }
+          { start: 0, end: 3, keysLinePos: 1 },
+          { start: 4, end: 6, keysLinePos: 1 },
+          { start: 7, end: 8, keysLinePos: 1 },
         ]
       })
-      const [videos, page] = parsed
+      const [videos, page, subjects] = parsed
       this.setState({
         loading: false,
         error: null,
         data: {
-          videos,
-          page: page[0]
+          videos: [...videos].sort((a, b) => {
+            const aDate = moment(a.date, 'DD/MM/YYYY')
+            const bDate = moment(b.date, 'DD/MM/YYYY')
+            return bDate - aDate
+          }),
+          page: page[0],
+          subjects
         }
       })
     }).catch(err => {
@@ -45,21 +75,42 @@ export default class Extrapol extends Component {
     })
   }
 
+  /* * * * * * * * * * * * * * * * * *
+   *
+   * RENDER
+   *
+   * * * * * * * * * * * * * * * * * */
   render () {
-    const { c } = this
+    const { c, state } = this
 
     const classes = [c]
-    console.log(this.state)
 
+    // DOM for loading and error states
+    if (state.loading) {
+      return <div className={classes.join(' ')}>
+        <div className={`${c}__loading`}>
+          <Loader />
+        </div>
+      </div>
+    } else if (state.loading) {
+      return <div className={classes.join(' ')}>
+        <div className={`${c}__error`}>
+          <LoadingError />
+        </div>
+      </div>
+    }
+
+    const { videos, page, subjects } = state.data
+    const heroStyle = { backgroundImage: `url(${page.image_url})` }
+
+    // DOM
     return <div className={classes.join(' ')}>
       <div className={`${c}__header`}>
-        <div className={`${c}__hero`}>
-          <PageTitle>Extrapol</PageTitle>
+        <div className={`${c}__hero`} style={heroStyle}>
+          <PageTitle>{page.page_title}</PageTitle>
         </div>
         <div className={`${c}__hat`}>
-          <Paragraph>
-            Chaque jour, de nouveaux Extras étaient à découvrir sur ExtraPol. Vous en avez raté un ? Retrouvez tous les anciens Extras publiés ici.
-          </Paragraph>
+          <Paragraph>{page.page_intro}</Paragraph>
         </div>
       </div>
       <div className={`${c}__links`}>
@@ -76,6 +127,25 @@ export default class Extrapol extends Component {
         </a>
       </div>
       <div className={`${c}__content`}>
+        {videos.map((vid, i, videos) => {
+          const currentSubject = subjects.find(sub => sub.date === vid.date)
+          const dayLabel = (i === 0 || vid.date !== videos[i - 1].date)
+            ? <div key={vid.date} className={`${c}__day-label`}>
+              <Slug>{moment(vid.date, 'DD/MM/YYYY').format('Do MMMM')}</Slug>
+              <BlockTitle>{currentSubject ? currentSubject.subject : 'Sans sujet'}</BlockTitle>
+            </div>
+            : ''
+          const videoSlot = <div key={vid.url}
+            className={`${c}__video-slot`}>
+            <VideoSlot url={vid.url} image={vid.image} />
+            <Annotation>{vid.list}</Annotation>
+          </div>
+          return [
+            dayLabel,
+            videoSlot
+          ].filter(e => e !== '')
+        })}
+        <LibeLaboLogo />
       </div>
     </div>
   }
---
+++ src/components/VideoSlot/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import Dailymotion from 'react-dailymotion'
+
+export default class VideoSlot extends Component {
+  constructor () {
+    super()
+    this.c = 'extrapol-videoslot'
+    this.state = {
+      active: false
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick (e) {
+    const { active } = this.state
+    if (!active) this.setState({ active: true })
+  }
+
+  render () {
+    const { c, props, state, handleClick } = this
+    const classes = [c]
+    const playerProps = {
+      height: '100%',
+      width: '100%',
+      video: props.url,
+      uiTheme: 'light',
+      paused: false,
+      autoplay: true
+    }
+    const dailyWrapperStyle = {
+      height: '100%',
+      width: '100%',
+      background: '#212121'
+    }
+    const imgPreviewStyle = {
+      backgroundImage: `url(${props.image})`
+    }
+    return <div onClick={handleClick} className={classes.join(' ')}>{
+      state.active
+        ? <div style={dailyWrapperStyle}><Dailymotion {...playerProps} /></div>
+        : <div style={imgPreviewStyle} />
+    }</div>
+  }
+}
